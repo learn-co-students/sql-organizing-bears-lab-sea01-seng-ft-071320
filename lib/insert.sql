3698caba663c48502fc9301ece40282c3d6a8cce
afteralec <github email address>
2020-07-21 17:10:01 -0700

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+VALUES
+  ('Mr. Chocolate', 37, 'M', 'white', 'cruel', 1);
+
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+VALUES
+  ('Rowdy', 37, 'M', 'white', 'cruel', 1);
+
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+VALUES
+  ('Tabitha', 37, 'M', 'white', 'cruel', 1);
+
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+VALUES
+  ('Sergeant Brown', 37, 'M', 'white', 'cruel', 1);
+
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+VALUES
+  ('Melissa', 37, 'M', 'white', 'cruel', 1);
+
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+VALUES
+  ('Grinch', 37, 'M', 'white', 'cruel', 1);
+
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+VALUES
+  ('Wendy', 37, 'M', 'white', 'cruel', 1);
+
+INSERT INTO bears
+  (name, age, gender, color, temperament, alive)
+VALUES
+  (NULL, 37, 'M', 'white', 'cruel', 1);
